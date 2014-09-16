d6453d8b9a657a2273c21a229e91a0eccf00bb88
Eric Smith <esmith@8thlight.com>
2014-09-16 09:29:00 -0700

    Add a crude version of the digital clock

--- src/main/java/Clock.java
+++ src/main/java/Clock.java
@@ -1,9 +1,28 @@
+import java.time.LocalTime;
+
 public class Clock {
 
-    public static void main(String [] args)
-    {
-        while(true) {
-            System.out.println("hello");
+    public static void main(String[] args) {
+        Clock clock = new Clock();
+        DigitalClock digitalClock = new DigitalClock();
+
+        while (true) {
+            digitalClock.Display(clock.getHours(), clock.getMinutes(), clock.getSeconds());
         }
+    }
+
+    public int getHours() {
+        LocalTime time = java.time.LocalTime.now();
+        return time.getHour();
+    }
+
+    public int getMinutes() {
+        LocalTime time = java.time.LocalTime.now();
+        return time.getMinute();
+    }
+
+    public int getSeconds() {
+        LocalTime time = java.time.LocalTime.now();
+        return time.getSecond();
     }
 }
---
+++ src/main/java/DigitalClock.java
@@ -0,0 +1,7 @@
+public class DigitalClock {
+
+    public void Display(int hours, int minutes, int seconds)
+    {
+        System.out.println(String.format("%d:%d:%d", hours, minutes, seconds));
+    }
+}
